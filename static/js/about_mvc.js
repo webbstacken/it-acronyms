f666e0e5e6922c0961e68a5d279781d77ec4c1df
webbstacken <glenn.wadstedt@protonmail.com>
2023-04-02 16:06:51 +0200

    .

--- static/js/about_mvc.js
+++ static/js/about_mvc.js
@@ -1,10 +1,12 @@
+var version=""$major.$minor.$build""
+var version="1.0.0";
 
 const addContent = (document) => {
   var text = document.getElementById("centerId").innerHTML;  
   text += '<div class="jumbotron jumbotron-fluid">';        
   text += '  <div class="container">';
   text += '    <h1 class="display-4">About...</h1>';
-  text += '      <p class="lead">Version (year.month.date.build#): ' + version +'</p>'
+  text += '      <p class="lead">Version (major.minor.epoc): ' + version +'</p>'
   text += '      <hr class="my-4">';
   text += '      <p class="lead">text text</p>'
   text += '    </div>';
