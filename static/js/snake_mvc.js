d401fe3fafac6e64ed6ec6eebeee61b16bb0b9a1
webbstacken <glenn.wadstedt@protonmail.com>
2023-04-04 14:38:49 +0200

    snake... seems to work ok

--- static/js/snake_mvc.js
+++ static/js/snake_mvc.js
@@ -12,107 +12,181 @@
 //     console.log("x: " + x + " y: " + y)
 //
 
-// Constants
+// Global constants
+const CANVAS_ID = "canvasId";
+const CANVAS_COLOR = "white";
+const CANVAS_HEIGHT = 300;
+const CANVAS_WIDTH = 350;
 const DIRECTIONS = {
   UP: 0,
   RIGHT:1,
   DOWN: 2,
   LEFT:3
 }
+const GAME_UPDATE_RATE = 100;
+const LEFT_BUTTON_ID = "leftButtonId";
+const RIGHT_BUTTON_ID = "rightButtonId"
+const SCOREBOARD_ID = "scoreId";
+const SNAKE_COLOR = "rgb(128, 128, 128)";
 const SNAKE_SEGMENT_SIZE = 5;
-const COLLISION_COLOR = 0;
-const CANVAS_ID = "canvas";
+const START_X = 95;
+const START_Y = 100;
+const TARGET_COLOR = "rgb(129, 129, 129)";
 
 // Global variables
 let direction = DIRECTIONS.RIGHT;
+let highScore = 0;
+let score = 0;
+let targetX = 0;  
+let targetY = 0;
 
-const updateGameBoard = (ctx, x, y) => {    
-  
-  ({x, y} = getNextPosition(x, y));    
-  
-  if (anyHit(ctx, x, y, COLLISION_COLOR)) {
-    alert("HIT!");
-  }   
-  ctx.fillRect(x, y, SNAKE_SEGMENT_SIZE, SNAKE_SEGMENT_SIZE);
-
-  return {"x": x, "y" : y};
+const addSnakeSegment = (ctx, x, y) => {
+    ctx.fillStyle = SNAKE_COLOR;
+    ctx.fillRect(x, y, SNAKE_SEGMENT_SIZE, SNAKE_SEGMENT_SIZE);
 }
 
-const anyHit = (ctx, x, y, hitColor) => {     
-  let sx = 0;     
-  let sy = 0;
-  let sw = 0;
-  let sh = 0;   
-        
-  switch (direction) {
-    case DIRECTIONS.UP:    sx = x;     sy = y - 6; sw = 5; sh = 4; break;    
-    case DIRECTIONS.RIGHT: sx = x + 6; sy = y;     sw = 4; sh = 5; break;
-    case DIRECTIONS.DOWN:  sx = x;     sy = y + 6; sw = 5; sh = 4; break;      
-    case DIRECTIONS.LEFT:  sx = x - 6; sy = y;     sw = 4; sh = 5; break;
+const addTarget = (ctx, init=false) => {
+  // Clear previous 
+  if (!init) {
+    ctx.fillStyle = CANVAS_COLOR;
+    ctx.fillRect(targetX, targetY, SNAKE_SEGMENT_SIZE*2, SNAKE_SEGMENT_SIZE*2);
   }
 
-  if (ctx.getImageData(sx, sy, sw, sh).data.some(color => color > hitColor)) {      
-      return true;
-  }
-  
-  return false;
+  // Add new target
+  updateFreeCordinateForTarget(ctx);
+  ctx.fillStyle = TARGET_COLOR;  
+  ctx.fillRect(targetX, targetY, SNAKE_SEGMENT_SIZE*2, SNAKE_SEGMENT_SIZE*2);
 }
 
-const getNextPosition = (x, y) => {
+const collision = (ctx, x, y, hitColor) => {    
+  const sx = x;     
+  const sy = y;
+  const sw = SNAKE_SEGMENT_SIZE;
+  const sh = SNAKE_SEGMENT_SIZE;   
+  let temp = ctx.getImageData(sx, sy, sw, sh).data;
+  console.log(temp);
+  return temp.some(color => color == hitColor) ? true: false;
+}
+
+const nextCordinate = (x, y) => {
   switch (direction) {
     case DIRECTIONS.UP: {    
-      y -= 5; break;
+      y -= SNAKE_SEGMENT_SIZE; break;
     }
     case DIRECTIONS.RIGHT: {        
-      x += 5;
+      x += SNAKE_SEGMENT_SIZE;
       break;
     }
     case DIRECTIONS.DOWN: {        
-      y += 5; 
+      y += SNAKE_SEGMENT_SIZE; 
       break;
     }
    case DIRECTIONS.LEFT: {         
-     x -= 5; 
+     x -= SNAKE_SEGMENT_SIZE; 
      break;
    }
   }
   return {"x": x, "y" : y};
 }
 
-// -------------------------------
-const initGame = (window) => {
-  const LOOP_DELAY = 100;  
-  const handle = document.getElementById("canvas");
+const segmentCollision = (ctx, x, y) => {
+  // TODO rgb 128, 128, 128     
+  return collision(ctx, x, y, 128);
+}
+
+const targetCollision = (ctx, x, y) => {
+  // TODO rgb 129, 129, 129     
+  return collision(ctx, x, y, 129);
+}
+
+const updateFreeCordinateForTarget = (ctx) => {
+  const padding = 10;
+  targetX = padding + getRandomNumber(CANVAS_WIDTH - 2*padding);
+  targetY = padding + getRandomNumber(CANVAS_HEIGHT - 2*padding);
   
-  let ctx = handle.getContext("2d");  
-  let x = 95;
-  let y = 100;
+  while(collision(ctx, targetX, targetY, 128)) {
+    targetX =  getRandomNumber(CANVAS_WIDTH);
+    targetY =  getRandomNumber(CANVAS_HEIGHT);
+  }  
+}
+
+const getRandomNumber = (maxValue) => {
+  return Math.floor(Math.random() * maxValue);
+}
+
+const updateGameBoard = (ctx, x, y) => {
+  // Get cordinate for next snake segment
+  ({x, y} = nextCordinate(x, y));    
+
+  if (wallCollision(x, y) || segmentCollision(ctx, x, y)) {
+    alert("Game Over!");
+    score = 0;
+    direction = DIRECTIONS.RIGHT;
+    x = START_X;
+    y = START_Y;
+    ctx.fillStyle = CANVAS_COLOR;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    addTarget(ctx);
+  }
+  else if (targetCollision(ctx, x, y)) {
+    updateScore();
+    addTarget(ctx);
+    addSnakeSegment(ctx, x, y); 
+  }
+  else {    
+    addSnakeSegment(ctx, x, y); 
+  }    
+
+  return {"x": x, "y" : y};
+}
+
+const updateScore = () => {
+  score++; 
+  highScore = score > highScore ?  score : highScore;
+  
+  let text = "Highscore: " + highScore + " Score: " + score;
+  text = (document.getElementById(SCOREBOARD_ID).innerHTML = text);
+}
+
+const wallCollision = (x, y) => {     
+  return  (x <= 0) || (x >= CANVAS_WIDTH) || (y <= 0) || (y >= CANVAS_HEIGHT) ? true: false;
+}
+
+// Default functions below
+const initGame = (window) => {    
+  const handle = document.getElementById(CANVAS_ID);  
+  const ctx = handle.getContext("2d");  
+  let x = START_X;
+  let y = START_Y;
+  
+  addTarget(ctx, true);
 
   window.setInterval(() => {
     ({x, y} = updateGameBoard(ctx, x, y));
-   }, LOOP_DELAY);     
+   }, GAME_UPDATE_RATE);     
 }
 
 const setupView = (document) => {
-  const canvasHeight = 300;
-  const canvasWidth = 350;
-
-  addView(document, canvasHeight, canvasWidth);      
+  addView(document);
 }
 
-const addView = (document, canvasHeight, canvasWidth) => {
+const addView = (document) => {
   let text = document.getElementById("centerId").innerHTML;  
   
   text += '<div class="container">';
   text += '  <div class="row">';
+
+  text += '    <div class="col-md-12">';
+  text += '      <p id="' + SCOREBOARD_ID + '">Highscore: 0 Score: 0</p>';  
+  text += '    </div>';
     
   text += '    <div class="col-md-12">';  
-  text += '      <canvas id="' + CANVAS_ID + '" height=' + canvasHeight  +'px" width="' + canvasWidth +'px" style="border: 1px solid #000000;" ></canvas>';    
+  text += '      <canvas id="' + CANVAS_ID + '" height=' + CANVAS_HEIGHT  +'px" width="' + CANVAS_WIDTH +'px" style="border: 1px solid #000000;" ></canvas>';    
   text += '    </div>';  
   
   text += '    <div class="col-md-12">';
-  text += '      <button type="button" style="width: 173px;" class="btn btn-outline-secondary">Left</button>';
-  text += '      <button type="button" style="width: 173px;" class="btn btn-outline-secondary">Right</button>';  
+  text += '      <button id="' + LEFT_BUTTON_ID + '"type="button" style="width: 173px;" class="btn btn-outline-secondary">Left</button>';
+  text += '      <button id="' + RIGHT_BUTTON_ID + '"type="button" style="width: 173px;" class="btn btn-outline-secondary">Right</button>';  
   text += '    </div>';
 
   text += '  </div>';  
@@ -122,24 +196,48 @@ const addView = (document, canvasHeight, canvasWidth) => {
 }
 
 const setupEventListener = (document) => {
-  const canvas = document.querySelector(CANVAS_ID)
+  function updateClockwiseDirection() {
+      if (direction == DIRECTIONS.LEFT) {
+        direction = DIRECTIONS.UP;
+      }
+      else {
+        direction += 1;
+      }
+  }
+
+  function updateCounterClockwiseDirection() {
+      if (direction == DIRECTIONS.UP) {
+        direction = DIRECTIONS.LEFT;
+      }
+      else {
+        direction -= 1;
+      }
+  }
+  
+  // left mouse click
+  const canvas = document.getElementById(CANVAS_ID)
   canvas.addEventListener("click", function(e) {   
-    if (direction == DIRECTIONS.UP){
-      direction = DIRECTIONS.LEFT;
-    }
-    else {
-      direction -= 1;
-     }
+    updateCounterClockwiseDirection();
      e.preventDefault();    
   });
 
+  // right mouse click
   canvas.addEventListener("contextmenu", function(e) {    
-    if (direction == DIRECTIONS.LEFT){
-      direction = DIRECTIONS.UP;
-    }
-    else {
-      direction += 1;
-    }
+    updateClockwiseDirection();
+    e.preventDefault();
+  });
+  
+  // left button...
+  const leftButton = document.getElementById(LEFT_BUTTON_ID)
+  leftButton.addEventListener("click", function(e) {   
+    updateCounterClockwiseDirection();
+    e.preventDefault();    
+  });
+  
+  // right button...
+  const rightButton = document.getElementById(RIGHT_BUTTON_ID)
+  rightButton.addEventListener("click", function(e) {      
+   updateClockwiseDirection()
     e.preventDefault();
   });
 }
