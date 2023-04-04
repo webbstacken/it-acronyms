41a6a33895bcb559a015085010419e6200f39f63
webbstacken <glenn.wadstedt@protonmail.com>
2023-04-04 08:22:15 +0200

    refactored snake

--- static/js/snake_mvc.js
+++ static/js/snake_mvc.js
@@ -1,93 +1,113 @@
-const LOOP_DELAY = 100;
+// https://medium.com/programming-essentials/how-to-pass-arguments-to-settimeout-and-setinterval-callbacks-520f13c47e58
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
+// https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+// https://www.geeksforgeeks.org/how-to-get-pixel-from-html-canvas/
+// const getCursorPosition = (canvas, event) => {
+//     const rect = canvas.getBoundingClientRect()
+//     const x = event.clientX - rect.left
+//     const y = event.clientY - rect.top
+//     console.log("x: " + x + " y: " + y)
+//
 
+// Constants
 const DIRECTIONS = {
   UP: 0,
   RIGHT:1,
   DOWN: 2,
   LEFT:3
 }
+const SNAKE_SEGMENT_SIZE = 5;
+const COLLISION_COLOR = 0;
+const CANVAS_ID = "canvas";
 
+// Global variables
 let direction = DIRECTIONS.RIGHT;
-let x = 95;
-let y = 100;
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
+const updateGameBoard = (ctx, x, y) => {    
+  
+  ({x, y} = getNextPosition(x, y));    
+  
+  if (anyHit(ctx, x, y, COLLISION_COLOR)) {
+    alert("HIT!");
+  }   
+  ctx.fillRect(x, y, SNAKE_SEGMENT_SIZE, SNAKE_SEGMENT_SIZE);
 
-// https://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
-// const getCursorPosition = (canvas, event) => {
-//     const rect = canvas.getBoundingClientRect()
-//     const x = event.clientX - rect.left
-//     const y = event.clientY - rect.top
-//     console.log("x: " + x + " y: " + y)
-// }
-
-
-const updateBoard = () => {
-  // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
-  const handle = document.getElementById("canvas");
-  let ctx = null;
-  let sx = 0;
-  let sy = 0;
-  let sw = 0;
-  let sh = 0;
-
-  if (handle.getContext) {
-    ctx = handle.getContext("2d");    
-        
-    switch (direction) {
-      case DIRECTIONS.UP: {
-        sx = x; sy = y - 6; sw = 5; sh = 4;
-        y -= 5; break;
-      }
-      case DIRECTIONS.RIGHT: {
-        sx = x + 6; sy = y; sw = 4; sh = 5;
-        x += 5;
-         break;
-      }
-      case DIRECTIONS.DOWN: {
-        sx = x; sy = y + 6; sw = 5; sh = 4;
-        y += 5; 
-        break;
-      }
-      case DIRECTIONS.LEFT: { 
-        sx = x - 6; sy = y; sw = 4; sh = 5;
-        x -= 5; 
-        break;
-      }
-    }   
-    
-    // https://www.geeksforgeeks.org/how-to-get-pixel-from-html-canvas/
-    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData
-    if (ctx.getImageData(sx, sy, sw, sh).data.some(color => color > 0)) {      
-      console.log("CRASH!");
-    }
-    else {
-      // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
-      ctx.fillRect(x, y, 5, 5);
-    }
-  }  
+  return {"x": x, "y" : y};
 }
 
+const anyHit = (ctx, x, y, hitColor) => {     
+  let sx = 0;     
+  let sy = 0;
+  let sw = 0;
+  let sh = 0;   
+        
+  switch (direction) {
+    case DIRECTIONS.UP:    sx = x;     sy = y - 6; sw = 5; sh = 4; break;    
+    case DIRECTIONS.RIGHT: sx = x + 6; sy = y;     sw = 4; sh = 5; break;
+    case DIRECTIONS.DOWN:  sx = x;     sy = y + 6; sw = 5; sh = 4; break;      
+    case DIRECTIONS.LEFT:  sx = x - 6; sy = y;     sw = 4; sh = 5; break;
+  }
 
-const initBoard = (window) => {
-  // https://medium.com/programming-essentials/how-to-pass-arguments-to-settimeout-and-setinterval-callbacks-520f13c47e58
+  if (ctx.getImageData(sx, sy, sw, sh).data.some(color => color > hitColor)) {      
+      return true;
+  }
+  
+  return false;
+}
+
+const getNextPosition = (x, y) => {
+  switch (direction) {
+    case DIRECTIONS.UP: {    
+      y -= 5; break;
+    }
+    case DIRECTIONS.RIGHT: {        
+      x += 5;
+      break;
+    }
+    case DIRECTIONS.DOWN: {        
+      y += 5; 
+      break;
+    }
+   case DIRECTIONS.LEFT: {         
+     x -= 5; 
+     break;
+   }
+  }
+  return {"x": x, "y" : y};
+}
+
+// -------------------------------
+const initGame = (window) => {
+  const LOOP_DELAY = 100;  
+  const handle = document.getElementById("canvas");
+  
+  let ctx = handle.getContext("2d");  
+  let x = 95;
+  let y = 100;
+
   window.setInterval(() => {
-    updateBoard()
+    ({x, y} = updateGameBoard(ctx, x, y));
    }, LOOP_DELAY);     
 }
 
 const setupView = (document) => {
-  addContent(document);      
+  const canvasHeight = 300;
+  const canvasWidth = 350;
+
+  addView(document, canvasHeight, canvasWidth);      
 }
 
-const addContent = (document) => {
+const addView = (document, canvasHeight, canvasWidth) => {
   let text = document.getElementById("centerId").innerHTML;  
   
   text += '<div class="container">';
   text += '  <div class="row">';
     
   text += '    <div class="col-md-12">';  
-  text += '      <canvas id="canvas" height="300px" width="350px" style="border: 1px solid #000000;" ></canvas>';    
+  text += '      <canvas id="' + CANVAS_ID + '" height=' + canvasHeight  +'px" width="' + canvasWidth +'px" style="border: 1px solid #000000;" ></canvas>';    
   text += '    </div>';  
   
   text += '    <div class="col-md-12">';
@@ -102,34 +122,30 @@ const addContent = (document) => {
 }
 
 const setupEventListener = (document) => {
-  const canvas = document.querySelector("canvas")
-
-  canvas.addEventListener("click", function(e) {
-    e.preventDefault();
-     
+  const canvas = document.querySelector(CANVAS_ID)
+  canvas.addEventListener("click", function(e) {   
     if (direction == DIRECTIONS.UP){
       direction = DIRECTIONS.LEFT;
     }
     else {
       direction -= 1;
-     }    
+     }
+     e.preventDefault();    
   });
 
-  canvas.addEventListener("contextmenu", function(e) {
-    // https://codinhood.com/nano/dom/disable-context-menu-right-click-javascript
-    e.preventDefault();
-    
+  canvas.addEventListener("contextmenu", function(e) {    
     if (direction == DIRECTIONS.LEFT){
       direction = DIRECTIONS.UP;
     }
     else {
       direction += 1;
-     }
+    }
+    e.preventDefault();
   });
 }
 
 export function initSnake(window, document) {  
   setupView(document);
   setupEventListener(document);
-  initBoard(window);  
+  initGame(window);  
 }
